7c62c06e7eb98341f628b1ba88705727717d1742
Guilherme Ganzarolli <guilhermegalmeida@hotmail.com>
2024-07-05 22:00:05 -0300

    incremento de algumas funções

--- src/funcoes.js
+++ src/funcoes.js
@@ -1,4 +1,23 @@
+let rodadas = 0
+
 export async function rollDice() {
     const diceValue = Math.floor(Math.random() * 6) +1
     return diceValue
+}
+
+
+export function corrida(participante1, participante2) {
+    console.log(`🏁🚨 Corrida entre ${participante1.nome} e ${participante2.nome} 🚨🏁`)
+    while (rodadas<6) {
+        console.log(rollDice())
+        console.log(rollDice())
+        console.log(`
+         ========== Rodada ${rodadas+1} ==========
+         Pista de -> 
+         Desempenho de ${participante1.nome} -> 
+         Desempenho de ${participante2.nome} -> 
+        `)
+        rodadas++
+    }
+    console.log('Programa Finalizado')
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import {mario, peach, yoshi, bowser, luigi, donkey} from './jogadores.js'
-import { rollDice } from './funcoes.js'
+import { rollDice, corrida } from './funcoes.js'
 
 (async function main(){
-    console.log(mario.nome)
-    console.log(await rollDice())
+    corrida(mario, luigi)
 })()
 
 // Fazer isso de colocar parênteses na função e depois
